dee61807b6f885e5672a4582bf5854c9da78ff68
Pikapikapi <huang.paladin@gmail.com>
2023-05-26 21:57:54 +0800

    feat: add users register password gensalt

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -16,8 +16,25 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  const { name, email, password } = req.body
-  User.create({ name, email, password }).then((user) => res.render('register'))
+  const { name, email, password, confirmpassword } = req.body
+
+  User.findOne({ where: { email } }).then((user) => {
+    if (user) {
+      console.log('user already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmpassword,
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hash) => User.create({ name, email, password: hash }))
+      .then(() => res.redirect('/'))
+      .catch((error) => console.log(error))
+  })
 })
 
 router.get('/logout', (req, res) => {
