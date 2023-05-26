b52193f0cdd0389c92c6cb8dd51f941be27e8c8e
Pikapikapi <huang.paladin@gmail.com>
2023-05-26 13:38:28 +0800

    feat: add new migration and add relationships between user and todo table

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
+/*
+ * 根據 models/index.js 裡的流程，Sequelize 會先進入 models 資料夾，
+ * 再透過 file system 去讀取資料夾內的特定 model，記得第一行要先載入資料夾。
+ */
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // setting body-parser
@@ -26,7 +34,8 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password } = req.body
+  User.create({ name, email, password }).then((user) => res.render('register'))
 })
 
 app.get('/users/logout', (req, res) => {
